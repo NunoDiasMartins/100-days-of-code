ced24ae2a83b1d59417cd8d34a051ee0cfaab6e7
Nuno Dias Martins <NunoDiasMartins@users.noreply.github.com>
2022-01-24 10:51:42 -0500

    Cleaning up the repo
    Moving all the code to the proper repos.
    code exercise was moved to https://github.com/NunoDiasMartins/coding-interview-solutions
    Small projects it was move to a private repo https://github.com/NunoDiasMartins/kekkonshiki

--- code solutions/move-element-to-end-q28-solution-2.ts
+++
@@ -1,5 +0,0 @@
-// O (n log(n))
-export function moveElementToEnd(array: number[], toMove: number) {
-    const sortFunction = (a: number, b: number) => a === toMove ? 1 : -1;
-    return array.sort(sortFunction);;
-}
